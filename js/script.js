2ad2dd6c447fa9fc4d57447e6a085eef71a0d3e3
Saref111 <saref012@gmail.com>
2022-05-17 19:30:03 +0300

    added repalying

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-const currentPlayer = 'X';
-const gameContainer = document.querySelector('.game');
-const gameControls = document.querySelectorAll('.game__control');
+const Players = {
+    X: 'X',
+    O: 'O'
+}
 const GameEndClassPrefixes = {
     X1: 'x1',
     X2: 'x2',
@@ -11,31 +12,43 @@ const GameEndClassPrefixes = {
     H2: 'h2',
     H3: 'h3',
 }
+let currentPlayer = Players.X;
+const gameContainer = document.querySelector('.game');
+const gameControls = document.querySelectorAll('.game__control');
+
+const changePlayer = () => currentPlayer = currentPlayer === Players.X ? Players.O : Players.X;
+const handleGameRestart = () => {
+    gameContainer.classList.remove(`game--end`);
+    gameContainer.classList.remove(`game--end-${GameEndClassPrefixes.X1}`, `game--end-${GameEndClassPrefixes.X2}`, `game--end-${GameEndClassPrefixes.V1}`, `game--end-${GameEndClassPrefixes.V2}`, `game--end-${GameEndClassPrefixes.V3}`, `game--end-${GameEndClassPrefixes.H1}`, `game--end-${GameEndClassPrefixes.H2}`, `game--end-${GameEndClassPrefixes.H3}`);
+    gameContainer.removeEventListener('click', handleGameRestart);
+    Array.from(gameControls).forEach((control) => control.querySelector('span').textContent = '');
+}
 const handleGameEnd = (prefix) => {
     const gameEndClass = `game--end-${prefix}`;
-
+    gameContainer.classList.add(`game--end`);
     gameContainer.classList.add(gameEndClass);
+    gameContainer.addEventListener('click', handleGameRestart);
 }
 const checkWinner = () => {
     const gameValues = [...gameControls].map((control) => control.querySelector('span').textContent.trim());
-
+    
     switch (true) {
-        case gameValues[0] === gameValues[1] && gameValues[1] === gameValues[2]:
+        case gameValues[0] === gameValues[1] && gameValues[1] === gameValues[2] && gameValues[0] !== '':
             handleGameEnd(GameEndClassPrefixes.H1);
             break;
-        case gameValues[3] === gameValues[4] && gameValues[4] === gameValues[5]:
+        case gameValues[3] === gameValues[4] && gameValues[4] === gameValues[5] && gameValues[3] !== '':
             handleGameEnd(GameEndClassPrefixes.H2);
             break;
-        case gameValues[6] === gameValues[7] && gameValues[7] === gameValues[8]:
+        case gameValues[6] === gameValues[7] && gameValues[7] === gameValues[8] && gameValues[6] !== '':
             handleGameEnd(GameEndClassPrefixes.H3);
             break;
-        case gameValues[0] === gameValues[3] && gameValues[3] === gameValues[6]:
+        case gameValues[0] === gameValues[3] && gameValues[3] === gameValues[6] && gameValues[0] !== '':
             handleGameEnd(GameEndClassPrefixes.V1);
             break;
-        case gameValues[1] === gameValues[4] && gameValues[4] === gameValues[7]:
+        case gameValues[1] === gameValues[4] && gameValues[4] === gameValues[7] && gameValues[1] !== '':
             handleGameEnd(GameEndClassPrefixes.V2);
             break;
-        case gameValues[2] === gameValues[5] && gameValues[5] === gameValues[8]:
+        case gameValues[2] === gameValues[5] && gameValues[5] === gameValues[8] && gameValues[2] !== '':
             handleGameEnd(GameEndClassPrefixes.V3);
             break;
         case gameValues[0] === gameValues[4] && gameValues[4] === gameValues[8]:
@@ -66,6 +79,7 @@ const checkGameState = () => {
 }
 
 const handleControlClick = (e) => {
+    e.stopPropagation();
     const { target } = e;
 
     if (target.textContent.trim()) {
@@ -75,5 +89,7 @@ const handleControlClick = (e) => {
     target.querySelector('span').textContent = currentPlayer;
 
     checkGameState();
+    changePlayer();
 }
+
 gameControls.forEach((control) => control.addEventListener('click', handleControlClick));
