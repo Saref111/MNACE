f88fc3b7a18539334f4322cd792db02f61587821
Saref111 <saref012@gmail.com>
2022-05-21 10:12:50 +0300

    added box class

---
+++ js/menace/box.js
@@ -0,0 +1,34 @@
+import { Players } from "../constants.js";
+
+export default class Box {
+    constructor(currentState) {
+        this.currentState = currentState;
+        this.possibleNextStates = [];
+        this.createNextStates();
+    }
+
+    createNextStates() {
+        for (let i = 0; i < this.currentState.length; i++) {
+            if (this.currentState[i] === '') {
+                const nextState = this.currentState.slice();
+                nextState[i] = this.getNextPlayer();
+                this.addNextState(new Box(nextState));
+            }
+        }
+    }
+
+    addNextState(nextState) {
+        const currentTurnNumber = this.getTurnNumber();
+        for (let i = 0; i < 10 - currentTurnNumber; i++) {
+            this.possibleNextStates.push(nextState);
+        }
+    }
+
+    getNextPlayer() {
+        return this.currentState.filter(state => state === '').length % 2 === 0 ? Players.O : Players.X;
+    }
+
+    getTurnNumber() {
+        return this.currentState.filter(state => state !== '').length;
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 import Game from './game.js';
+import Box from './menace/box.js';
 
 const game = new Game();
+
+const boxesTree = new Box(['', '', '', '', '', 'X', '', '', '']);
+console.log(boxesTree);
